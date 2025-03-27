35434f4c67c2c6199e413305a07b6cfbac6d486d
renatha <910161@csglq6l7_910161.telkom.co.id>
2025-03-27 22:41:53 +0700

    create project

---
+++ src/main/java/mampu/io/MampuApplication.java
@@ -0,0 +1,13 @@
+package mampu.io;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MampuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MampuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mampu/io/MampuApplicationTests.java
@@ -0,0 +1,13 @@
+package mampu.io;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MampuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
