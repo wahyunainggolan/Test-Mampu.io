73946589a1f50632417d4ace69714017b9f9fbb8
renatha <910161@csglq6l7_910161.telkom.co.id>
2025-03-28 02:10:25 +0700

    backup database

---
+++ database/query.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (username, email) VALUES ('user1', 'user1@example.com');
+INSERT INTO users (username, email) VALUES ('user2', 'user2@example.com');
+
+INSERT INTO wallets (balance, user_id) VALUES (1000.00, 1);
+INSERT INTO wallets (balance, user_id) VALUES (500.00, 2);
